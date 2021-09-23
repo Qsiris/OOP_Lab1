cf0f218739766e1469cfa145bc3d161d9817c358
Maxim <Maxim@DESKTOP-L31BS0L>
2021-09-23 23:27:08 +0300

    Дописал комменты

--- OOP_Lab1/IntDinamicArray.cpp
+++ OOP_Lab1/IntDinamicArray.cpp
@@ -4,23 +4,31 @@
 
 #include "IntDinamicArray.h"
 
+//  онструктор по умолчанию дл€ динамического массива
+// обнул€ет размер массива и указатель на него
+// ради инициализации всех полей класса
 IntDinamicArray::IntDinamicArray()
 {
     this->array_ = nullptr;
     this->size_ = 0;
 }
 
+//  онструктор с параметром размер массива
+// создает массив указанного размера
 IntDinamicArray::IntDinamicArray(int size)
 {
     this->array_ = new int[size];
     this->size_ = size;
 }
 
+// √еттер дл€ получени€ размера массива
 int IntDinamicArray::GetSize()
 {
     return this->size_;
 }
 
+// ѕроцедура изменени€ размера массива
+// принемает размер нового массива
 void IntDinamicArray::Resize(int new_size)
 {
     this->array_ = new int[new_size];
@@ -31,11 +39,16 @@ IntDinamicArray::~IntDinamicArray()
 {
 }
 
+// √еттер дл€ возврата элемента 
+// динамического массива по его индексу.
+// параметр индекс, по которому находитс€ элемент в массиве
 int IntDinamicArray::GetNumberFromIndex(int index)
 {
     return this->array_[index];
 }
 
+// —еттер позвол€ющий присвоить элементу по индексу новое значение
+// дл€ этого принимает в параметры сам элемент и его индекс
 void IntDinamicArray::SetNumberByIndex(int element, int index)
 {
     this->array_[index] = element;
--- OOP_Lab1/NumberInModSystem.cpp
+++ OOP_Lab1/NumberInModSystem.cpp
@@ -7,11 +7,14 @@
 #include "NumberInModSystem.h"
 using namespace std;
 
+// Вовзращает число, переведенное в
+// систему остаточных классов в десятичном виде
 int NumberInModSystem::GetNumber()
 {
     return this->number_;
 }
 
+// Выводит на экран число в системе остаточных классов
 void NumberInModSystem::PrintArray()
 {
     for (int i = 0; i < div_array_.GetSize(); i++)
@@ -21,31 +24,45 @@ void NumberInModSystem::PrintArray()
     cout << endl;
 }
 
+// Выводит на экран число, переведенное в
+// систему остаточных классов в десятичном виде
 void NumberInModSystem::PrintNumber()
 {
     cout << number_ << endl;
 }
 
+// Устанавливает значение в остаточный класс 
+// (запись числа в определенную ячейку по индексу)
 void NumberInModSystem::SetElementByIndex(int element, int index)
 {
     div_array_.SetNumberByIndex(element, index);
 }
 
+// Возвращает элемент остаточного класса
+// (возврат числа из определенной ячейки по индексу)
 int NumberInModSystem::GetArrayElementFromIndex(int index)
 {
     return div_array_.GetNumberFromIndex(index);
 }
 
+// Конструктор по умолчанию, который обнуляет число в десятичном виде
+// переводимое в систему остаточных классов
+// ради избежания непредвиденного поведения
 NumberInModSystem::NumberInModSystem()
 {
     this->number_ = 0;
 }
 
+// Конструктор с параметром строкой (число, введенное как строка
+// переводится сначала в сишную строку, а затем в инт)
 NumberInModSystem::NumberInModSystem(string number)
 {
     this->number_ = atoi(number.c_str());
 }
 
+// Конструктор с переметром динамический массив числа в остаточной системе счисления
+// копирует массив и заодно переодит число из системы остаточных классов в 
+// десятичную систему счисления
 NumberInModSystem::NumberInModSystem(class IntDinamicArray array_input)
 {
     this->number_ = 0;
